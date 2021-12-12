7fbc78e6a1b7fa76194f5426acc636841f2dea3d
Rahul Makhija <rahulmakhija02@gmail.com>
2021-12-13 03:24:14 +0530

    DB connection

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ const cors = require('cors');
 const app = express();
 const port = 3000;
 
-// mongoose.connect(process.env.DB_Connect, { useNewUrlParser: true }, () =>
-//   console.info("Connected to Mongo Db")
-// );
+mongoose.connect(process.env.DB_Connect, { useNewUrlParser: true }, () =>
+  console.info("Connected to Mongo Db")
+);
 app.use(express.static('APOD'));
 app.use(cors());
 // app.use("/planetary", apodRoutes);
